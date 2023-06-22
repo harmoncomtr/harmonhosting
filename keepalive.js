be596c98bbf1f1b8760aa2eb5ed6e08cb683813f
Harmon Developer <137448637+harmoncomtr@users.noreply.github.com>
2023-06-22 20:40:39 +0000

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -1,6 +1,12 @@
+const express = require("express");
 const http = require("http");
+const app = express();
 
 const server = http.createServer(function (req, res) {
   res.write("Harmon.com.tr - Aktif Tutuluyor.");
   res.end();
 }).listen(8080);
+
+// This is the important stuff
+server.keepAliveTimeout = (60 * 1000) + 1000;
+server.headersTimeout = (60 * 1000) + 2000;
